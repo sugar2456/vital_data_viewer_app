df8c4eb2b4bd9a25b7cc523feb37b7ad802089e5
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-03-27 08:08:05 +0900

    活動サマリーを取得

--- lib/repositories/impls/activity_repository_impl.dart
+++ lib/repositories/impls/activity_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:vital_data_viewer_app/models/response/activity_summary_response.dart';
 import 'package:vital_data_viewer_app/repositories/interfaces/activity_repository_interface.dart';
 import 'package:vital_data_viewer_app/models/response/activity_goal_response.dart';
 import 'package:vital_data_viewer_app/util/header_util.dart';
@@ -22,6 +23,27 @@ class ActivityRepositoryImpl implements ActivityGoalRepositoryInterface {
     } catch (e) {
       print(e);
       throw Exception('Failed to load activity goal');
+    }
+  }
+
+  @override
+  Future<ActivitySummaryResponse> fetchActivitySummary() async {
+    // 今日の日付 yyyy-MM-dd
+    final date = DateTime.now().toIso8601String().substring(0, 10);
+    final uri = Uri.https('api.fitbit.com', '/1/user/-/activities/date/$date.json');
+
+    try {
+      final response = await http.get(uri, headers: HeaderUtil.createAuthHeaders());
+      if (response.statusCode == 200) {
+        final responseBody = json.decode(response.body);
+        return ActivitySummaryResponse.fromJson(responseBody);
+      } else {
+        print(response.statusCode);
+        throw Exception('Failed to load activity summary');
+      }
+    } catch (e) {
+      print(e);
+      throw Exception('Failed to load activity summary');
     }
   }
 }
--- lib/view_models/home_view_model.dart
+++ lib/view_models/home_view_model.dart
@@ -6,7 +6,7 @@ import 'package:vital_data_viewer_app/models/response/device_response.dart';
 import 'package:vital_data_viewer_app/repositories/interfaces/activity_repository_interface.dart';
 import 'package:vital_data_viewer_app/repositories/interfaces/body_goal_repository_interface.dart';
 import 'package:vital_data_viewer_app/repositories/interfaces/device_repository_interdace.dart';
-
+import 'package:vital_data_viewer_app/models/response/activity_summary_response.dart';
 class HomeViewModel extends ChangeNotifier {
   final ActivityGoalRepositoryInterface _activityGoalRepository;
   final BodyGoalRepositoryInterface _bodyGoalRepository;
@@ -16,30 +16,32 @@ class HomeViewModel extends ChangeNotifier {
   AcitivityGoalResponse? _activityGoalResponse;
   AcitivityGoalResponse? get activityGoalResponse => _activityGoalResponse;
 
+  ActivitySummaryResponse? _activitySummaryResponse;
+  ActivitySummaryResponse? get activitySummaryResponse => _activitySummaryResponse;
+
   BodyGoalResponse? _bodyGoalResponse;
   BodyGoalResponse? get bodyGoalResponse => _bodyGoalResponse;
 
   DeviceResponse? _deviceResponse;
   DeviceResponse? get deviceResponse => _deviceResponse;
 
-  bool _isLoading = false;
-  bool get isLoading => _isLoading;
+  // bool _isLoading = false;
+  // bool get isLoading => _isLoading;
 
   Future<void> getActivityGoal() async {
-    _isLoading = true;
-    notifyListeners();
+    // _isLoading = true;
+    // notifyListeners();
 
     try {
-      final response = await _activityGoalRepository.fetchActivityGoal();
-      _activityGoalResponse = response;
-
+      _activitySummaryResponse = await _activityGoalRepository.fetchActivitySummary();
+      _activityGoalResponse = await _activityGoalRepository.fetchActivityGoal();
       _bodyGoalResponse = await _bodyGoalRepository.fetchBodyGoal();
       _deviceResponse = await _deviceRepository.fetchDevice();
     } catch (e) {
       print('Error fetching activity goal: $e');
     } finally {
-      _isLoading = false;
-      notifyListeners();
+      // _isLoading = false;
+      // notifyListeners();
     }
   }
 
