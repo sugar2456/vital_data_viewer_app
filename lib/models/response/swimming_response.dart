1b4a864a53a4e7fe182721fe18236c84d99f7f04
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-03-30 21:42:14 +0900

    水泳情報のレスポンスクラスの作成

---
+++ lib/models/response/swimming_response.dart
@@ -0,0 +1,64 @@
+import 'package:vital_data_viewer_app/models/response/dataset.dart';
+
+class SwimmingResponse {
+  final List<ActivitySwimmingStroke> activitiesSwimmingStroke;
+  final ActivitySwimmingStrokeIntraday activitiesSwimmingStrokeIntraday;
+
+  SwimmingResponse({
+    required this.activitiesSwimmingStroke,
+    required this.activitiesSwimmingStrokeIntraday,
+  });
+
+  factory SwimmingResponse.fromJson(Map<String, dynamic> json) {
+    final baseDate = (json['activities-swimming-stroke'] as List)
+        .map((item) => ActivitySwimmingStroke.fromJson(item))
+        .first
+        .dateTime;
+    return SwimmingResponse(
+      activitiesSwimmingStroke: (json['activities-swimming-stroke'] as List)
+          .map((item) => ActivitySwimmingStroke.fromJson(item))
+          .toList(),
+      activitiesSwimmingStrokeIntraday:
+          ActivitySwimmingStrokeIntraday.fromJson(json['activities-swimming-stroke-intraday'], baseDate),
+    );
+  }
+}
+
+class ActivitySwimmingStroke {
+  final String dateTime;
+  final int value;
+
+  ActivitySwimmingStroke({
+    required this.dateTime,
+    required this.value,
+  });
+
+  factory ActivitySwimmingStroke.fromJson(Map<String, dynamic> json) {
+    return ActivitySwimmingStroke(
+      dateTime: json['dateTime'],
+      value: int.parse(json['value']),
+    );
+  }
+}
+
+class ActivitySwimmingStrokeIntraday {
+  final List<Dataset> dataset;
+  final int datasetInterval;
+  final String datasetType;
+
+  ActivitySwimmingStrokeIntraday({
+    required this.dataset,
+    required this.datasetInterval,
+    required this.datasetType,
+  });
+
+  factory ActivitySwimmingStrokeIntraday.fromJson(Map<String, dynamic> json, String baseDate) {
+    return ActivitySwimmingStrokeIntraday(
+      dataset: (json['dataset'] as List)
+          .map((item) => Dataset.fromJson(item, baseDate))
+          .toList(),
+      datasetInterval: json['datasetInterval'],
+      datasetType: json['datasetType'],
+    );
+  }
+}
