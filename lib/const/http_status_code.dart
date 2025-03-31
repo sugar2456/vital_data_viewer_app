8a4431704f8ff063b000a0c70bead0a95ccf9dc2
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-01 01:48:15 +0900

    ステータスコードの定義

---
+++ lib/const/http_status_code.dart
@@ -0,0 +1,16 @@
+class HttpStatusCode {
+  static const int ok = 200;
+  static const int created = 201;
+  static const int accepted = 202;
+  static const int noContent = 204;
+  static const int movedPermanently = 301;
+  static const int found = 302;
+  static const int notModified = 304;
+  static const int badRequest = 400;
+  static const int unauthorized = 401;
+  static const int forbidden = 403;
+  static const int notFound = 404;
+  static const int methodNotAllowed = 405;
+  static const int tooManyRequests = 429;
+  static const int internalServerError = 500;
+}
