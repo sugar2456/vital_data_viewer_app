52b13ddaa7cf925096037f358209cac1c3f9f4db
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-04 10:58:25 +0900

    小数点二桁の数字を四捨五入する関数を実装

--- lib/util/convert_util.dart
+++ lib/util/convert_util.dart
@@ -13,4 +13,17 @@ class ConvertUtil {
       throw ArgumentError('Convert Util 不正な値が渡されました: $value');
     }
   }
+
+  /// 小数点第2位以下までの数値を四捨五入する
+  static double roundToOneDecimalPlaces(double value) {
+    if (value.isNaN || value.isInfinite) {
+      throw ArgumentError('Convert Util 不正な値が渡されました: $value');
+    }
+    try {
+      return double.parse(value.toStringAsFixed(1));
+    } catch (e) {
+      log('Error rounding to two decimal places: $e');
+      throw ArgumentError('Convert Util 不正な値が渡されました: $value');
+    }
+  }
 }
--- test/util/convert_util_test.dart
+++ test/util/convert_util_test.dart
@@ -31,4 +31,35 @@ void main() {
       expect(ConvertUtil.stringToInt('0'), 0);
     });
   });
+
+  group('ConvertUtil.roundToOneDecimalPlaces', () {
+    test('整数を渡すとそのまま返す', () {
+      expect(ConvertUtil.roundToOneDecimalPlaces(123), 123.0);
+    });
+
+    test('小数点以下1桁の数値を渡すとそのまま返す', () {
+      expect(ConvertUtil.roundToOneDecimalPlaces(123.4), 123.4);
+    });
+
+    test('小数点以下2桁以上の数値を渡すと四捨五入される', () {
+      expect(ConvertUtil.roundToOneDecimalPlaces(123.45), 123.5);
+    });
+
+    test('負の数値の小数点以下1桁の数値を渡すとそのまま返す', () {
+      expect(ConvertUtil.roundToOneDecimalPlaces(-123.4), -123.4);
+    });
+
+    test('負の数値の小数点以下2桁以上の数値を渡すと四捨五入される', () {
+      expect(ConvertUtil.roundToOneDecimalPlaces(-123.45), -123.5);
+    });
+
+    test('0を渡すと0を返す', () {
+      expect(ConvertUtil.roundToOneDecimalPlaces(0), 0.0);
+    });
+
+    test('なんらかの理由で無効な値が渡された場合、ArgumentError をスローする', () {
+      expect(() => ConvertUtil.roundToOneDecimalPlaces(double.nan), throwsA(isA<ArgumentError>()));
+      expect(() => ConvertUtil.roundToOneDecimalPlaces(double.infinity), throwsA(isA<ArgumentError>()));
+    });
+  });
 }
