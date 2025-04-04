965aa81b6669f2cf5ffffc0e269541ece8d28d0a
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-04 18:11:41 +0900

    タイトル名を修正

--- lib/main.dart
+++ lib/main.dart
@@ -45,7 +45,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Vital Data Viewer App',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
