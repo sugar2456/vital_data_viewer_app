8ffd02a36ff2092fccf30e6258684188f6fe19c6
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-12 04:03:24 +0900

    ログイン画面の認証失敗時のエラー処理を実装

--- lib/repositories/impls/login_repository_impl.dart
+++ lib/repositories/impls/login_repository_impl.dart
@@ -1,3 +1,5 @@
+import 'package:vital_data_viewer_app/const/http_status_code.dart';
+import 'package:vital_data_viewer_app/exceptions/external_service_exception.dart';
 import 'package:vital_data_viewer_app/models/response/login_response.dart';
 import '../interfaces/login_repository_interface.dart';
 import 'package:flutter_web_auth_2/flutter_web_auth_2.dart';
@@ -6,17 +8,21 @@ class LoginRepositoryImpl implements LoginRepositoryInterface {
   String callbackUrl = 'myapp://callback';
   @override
   Future<LoginResponse> login(String clientId) async {
-    final url = Uri.https('www.fitbit.com', '/oauth2/authorize', {
-      'response_type': 'token',
-      'client_id': clientId,
-      'redirect_uri': callbackUrl,
-      'scope': 'activity heartrate location nutrition profile settings sleep weight',
-    });
-    final result = await FlutterWebAuth2.authenticate(url: url.toString(), callbackUrlScheme: 'myapp');
-    final fragment = Uri.parse(result).fragment;
-    final fragmentParams = Uri.splitQueryString(fragment);
-    final token = fragmentParams['access_token'];
+    try {
+      final url = Uri.https('www.fitbit.com', '/oauth2/authorize', {
+        'response_type': 'token',
+        'client_id': clientId,
+        'redirect_uri': callbackUrl,
+        'scope': 'activity heartrate location nutrition profile settings sleep weight',
+      });
+      final result = await FlutterWebAuth2.authenticate(url: url.toString(), callbackUrlScheme: 'myapp');
+      final fragment = Uri.parse(result).fragment;
+      final fragmentParams = Uri.splitQueryString(fragment);
+      final token = fragmentParams['access_token'];
 
-    return LoginResponse(token: token ?? "" , tokenType: 'Bearer');
+      return LoginResponse(token: token ?? "" , tokenType: 'Bearer');
+    } catch (e) {
+      throw ExternalServiceException(e.toString(), '認証に失敗しました', HttpStatusCode.unauthorized);
+    }
   }
 }
