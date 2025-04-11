37c1847db6c934cc4e77c890c525064e692cf906
satoru.ogawa <sugar2456_0526@yahoo.co.jp>
2025-04-12 05:28:33 +0900

    水泳リポジトリの修正

--- lib/repositories/impls/swimming_repository_impl.dart
+++ lib/repositories/impls/swimming_repository_impl.dart
@@ -1,16 +1,23 @@
 import 'package:http/http.dart' as http;
 import 'package:vital_data_viewer_app/models/response/swimming_response.dart';
 import 'package:vital_data_viewer_app/repositories/interfaces/swimming_repository_interface.dart';
-import 'package:vital_data_viewer_app/util/header_util.dart';
-import 'package:vital_data_viewer_app/util/http_util.dart';
+import 'package:vital_data_viewer_app/repositories/impls/base_request_class.dart';
 
-class SwimmingRepositoryImpl extends SwimmingRepositoryInterface {
+class SwimmingRepositoryImpl extends BaseRequestClass
+    implements SwimmingRepositoryInterface {
+  final Map<String, String> headers;
+  final http.Client client;
+  SwimmingRepositoryImpl({
+    required this.headers,
+    required this.client,
+  }) : super(client: client);
+
   @override
-  Future<SwimmingResponse> fetchSwimming(String date, String detailLevel) async {
-    final uri = Uri.https('api.fitbit.com', '/1/user/-/activities/swimming-strokes/date/$date/1d/$detailLevel.json');
-    final headers = HeaderUtil.createAuthHeaders();
-    final httpUtil = HttpUtil(client: http.Client());
-    final responseBody = await httpUtil.get(uri, headers);
+  Future<SwimmingResponse> fetchSwimming(
+      String date, String detailLevel) async {
+    final uri = Uri.https('api.fitbit.com',
+        '/1/user/-/activities/swimming-strokes/date/$date/1d/$detailLevel.json');
+    final responseBody = await super.get(uri, headers);
     return SwimmingResponse.fromJson(responseBody);
   }
 }
